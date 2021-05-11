e655166b7d26159291bb8095e684be28f94b1807
Alan Gomes <alan.gomes@g.globo>
2021-05-11 20:45:23 -0300

    finished authorization log

--- decorators.go
+++ decorators.go
@@ -22,33 +22,37 @@ func respondNotAuthorized(w http.ResponseWriter, r *http.Request) {
 		Message: "Not-Authorized",
 		Data:    make(map[string]interface{}),
 	}
-	logWarn.Printf("%s - FhaaS received a request that was not authorized. It may be a token/url mistake but also may have came from an undesired/malicious requester. We suggest you to check your FhaaS network entrypoints\n", getRequestId(w))
-	logError.Printf("%s - This request was not authorized by endpoint %s\n", getRequestId(w), getAuthUrlUsed(w))
+
 	respond(env, w, 401)
 }
 
 func verifyAuth(endpoint func(http.ResponseWriter, *http.Request)) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		reqId := getRequestId(w)
 		fmt.Println("Checking authorization")
 		authToken := r.Header.Get(H_AUTH_TOKEN)
 		authUrlHeader := r.Header.Get(H_AUTH_URL)
 		authUrl := selectAuthUrl(authUrlHeader)
 		w.Header().Set(H_AUTH_URL_USED, authUrl)
 		fmt.Printf("AuthUrl used: %s\n", authUrl)
-		logDebug.Printf("%s - Auth URL used for this request: \n", getRequestId(w))
+		logDebug.Printf("%s - Auth URL used for this request: %s\n", reqId, authUrl)
 		if authToken != "" && authUrl != "" {
 			fmt.Println("Trying to auth")
-			_, code, err := doPost(authUrl, fmt.Sprintf("%s", authToken), getAuthContentType(w))
+			resp, code, err := doPost(authUrl, fmt.Sprintf("%s", authToken), getAuthContentType(w))
 			if err != nil {
 				fmt.Println(err)
-				logError.Printf("%s - Authentication process failed: %v", getRequestId(w), err)
+				logError.Printf("%s - Authentication process failed: %v", reqId, err)
 			}
 			if code != 200 {
 				fmt.Printf("Not authorized, status code: %d\n", code)
+				logWarn.Printf("%s - FhaaS received a request that was not authorized. It may be a token/url mistake but also may have came from an undesired/malicious requester. We suggest you to check your FhaaS network entrypoints\n", reqId)
+				logError.Printf("%s - This request was not authorized by endpoint %s\n", reqId, authUrl)
+				logDebug.Printf("%s - Authorization response: %s\n", reqId, resp)
+				logDebug.Printf("%s - Authorization response code: %d\n", reqId, code)
 				respondNotAuthorized(w, r)
 			} else {
 				fmt.Println("Authorized")
-				logError.Printf("%s - Authorized. Processing request.", getRequestId(w))
+				logError.Printf("%s - Authorized. Processing request.", reqId)
 				endpoint(w, r)
 			}
 		} else {
